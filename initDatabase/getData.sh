b6ee196b3776661e543ba291a3806427c9e693f1
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-11-28 16:51:03 -0300

    get data now works from everywhere

--- initDatabase/getData.sh
+++ initDatabase/getData.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cd $BASH_SOURCE
+if [ "$(uname)" != "Linux" ]; then
+    realpath() {
+        [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+    }
+fi
+cd $(dirname $(realpath $BASH_SOURCE))
 mkdir -p data
 cd data
 wget "https://telegramas.resultados.gob.ar/MESAS_ESCRUTADAS_Cierre.zip"
