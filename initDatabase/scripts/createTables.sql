78b6c41bfd3bee5dcc4a1452e64eb5cd226983fe
Agustín Roca <aroca@itba.edu.ar>
2021-12-04 00:04:10 -0300

    SQL WIP

--- initDatabase/scripts/createTables.sql
+++ initDatabase/scripts/createTables.sql
@@ -1,4 +1,5 @@
 CREATE TABLE IF NOT EXISTS votos_aux(
+    id SERIAL PRIMARY KEY,
     Agrupacion TEXT,
     Cargo TEXT,
     Codigo TEXT,
@@ -19,40 +20,91 @@ CREATE TABLE IF NOT EXISTS votos_aux(
     votos TEXT
 );
 
+
+
 CREATE TABLE IF NOT EXISTS cargos(
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL
 );
 
 
-CREATE TABLE IF NOT EXISTS agrupacion(
+CREATE TABLE IF NOT EXISTS agrupaciones(
     id INTEGER PRIMARY KEY,
-    idText TEXT,
+    idText TEXT, --No tiene utilidad?
     name TEXT NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS provincias(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS secciones(
+    id SERIAL PRIMARY KEY,
+    idBd INTEGER, --No tiene utilidad?
+    idProvincia INTEGER, 
+    name TEXT NOT NULL,
+
+    FOREIGN KEY(idProvincia) REFERENCES provincias
+
+);
+
+CREATE TABLE IF NOT EXISTS circuitos(
+    id SERIAL PRIMARY KEY,
+    idBd TEXT,
+    idSeccion INTEGER,
+    
+    FOREIGN KEY(idSeccion) REFERENCES secciones
+
+);
+
+CREATE TABLE IF NOT EXISTS establecimientos(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    idCircuito INTEGER,
+    codigo INTEGER, 
+    
+    FOREIGN KEY(idCircuito) REFERENCES circuitos
+
+    
+);
+
+CREATE TABLE IF NOT EXISTS envios(
+    id INTEGER PRIMARY KEY,
+    date TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS mesas(
+    id SERIAL PRIMARY KEY,
+    idBd TEXT,
+    idEstablecimiento INTEGER,
+    electores INTEGER,
+    idEnvio INTEGER,
+    
+    FOREIGN KEY(idEstablecimiento) REFERENCES establecimientos,
+    FOREIGN KEY(idEnvio) REFERENCES envios
+);
+
+
+
+
+CREATE TABLE IF NOT EXISTS tiposvotos(
+    id SERIAL PRIMARY KEY,
+    name TEXT
+);
 
 CREATE TABLE IF NOT EXISTS votos(
     id SERIAL PRIMARY KEY,
 
     idAgrupacion INTEGER ,
     idCargo INTEGER,
+    idMesa INTEGER,
+    idTipoVoto INTEGER,
+    
+    votes INTEGER,
 
-    Codigo TEXT,
-    Distrito TEXT,
-    Establecimiento TEXT,
-    Fecha TIMESTAMP,
-    IdCircuito TEXT,
-    IdDistrito TEXT,
-    IdSeccion TEXT,
-    Mesa TEXT,
-    Seccion TEXT,
-    electores TEXT,
-    envio TEXT,
-    tipoVoto TEXT,
-    votos TEXT,
-
-    FOREIGN KEY(idAgrupacion) REFERENCES agrupacion,
-    FOREIGN KEY(idCargo) REFERENCES cargos
+    FOREIGN KEY(idAgrupacion) REFERENCES agrupaciones,
+    FOREIGN KEY(idMesa) REFERENCES mesas,
+    FOREIGN KEY(idCargo) REFERENCES cargos,
+    FOREIGN KEY(idTipoVoto) REFERENCES tiposvotos
 );
-
--- initDatabase/scripts/posInsertProcess.sql
+++ initDatabase/scripts/posInsertProcess.sql
@@ -1,46 +1,100 @@
 INSERT INTO cargos (id, name)
-SELECT DISTINCT(CAST(idCargo AS INTEGER)),
+SELECT DISTINCT(CAST(idCargo AS INTEGER)), 
     Cargo
 FROM votos_aux
 WHERE idCargo != '';
 
-INSERT INTO agrupacion (id, idText, name)
+INSERT INTO agrupaciones (id, idText, name)
 SELECT DISTINCT(CAST(idAgrupacionInt AS INTEGER)),
     idAgrupacion,
     agrupacion
 FROM votos_aux
 WHERE idAgrupacionInt != '';
 
-INSERT INTO votos (
-        idAgrupacion,
-        idCargo,
-        Codigo,
-        Distrito,
-        Establecimiento,
-        Fecha,
-        IdCircuito,
-        IdDistrito,
-        IdSeccion,
-        Mesa,
-        Seccion,
-        electores,
-        envio,
-        tipoVoto,
-        votos
-    )
-SELECT CAST(idAgrupacionInt AS INTEGER),
-    CAST(idCargo AS INTEGER),
-    Codigo,
-    Distrito,
-    Establecimiento,
-    Fecha,
-    IdCircuito,
-    IdDistrito,
-    IdSeccion,
-    Mesa,
-    Seccion,
-    electores,
-    envio,
-    tipoVoto,
-    votos
-FROM votos_aux;
+INSERT INTO provincias (id, name)
+SELECT DISTINCT(CAST(IdDistrito AS INTEGER)),
+    Distrito
+FROM votos_aux
+WHERE IdDistrito != '';
+
+INSERT INTO secciones (idBD, idProvincia, name)
+    SELECT DISTINCT CAST(IdSeccion AS INTEGER),
+    CAST(IdDistrito AS INTEGER),
+    Seccion
+FROM votos_aux
+WHERE IdSeccion != '';
+
+INSERT INTO circuitos (idBD, idSeccion)
+    SELECT DISTINCT IdCircuito,
+    s.id
+FROM votos_aux a JOIN secciones s ON CAST(a.IdSeccion AS INTEGER) = s.idBD AND CAST(a.IdDistrito AS INTEGER) = s.idProvincia
+WHERE IdCircuito != '';
+
+
+
+WITH newS AS (
+    SELECT id AS idsecNoConflict,
+        idBd,
+        idProvincia
+    FROM secciones
+),
+newC AS (
+    SELECT id AS idcirNoConflict,
+        idBd,
+        idSeccion
+    FROM circuitos
+)
+INSERT INTO establecimientos (name, idCircuito, codigo)
+select 
+    establecimiento,
+idcirNoConflict,
+    CAST(Codigo AS INTEGER)
+from (select idcirNoConflict, establecimiento, codigo
+from (
+        SELECT *
+        FROM (
+                (
+                    select idDistrito,
+                        idSeccion,
+                        idCircuito,
+                        establecimiento,
+                        codigo
+                    from votos_aux
+                    group by idDistrito,
+                        idSeccion,
+                        idCircuito,
+                        establecimiento,
+                        codigo
+                ) as A
+                LEFT JOIN newS s ON s.idProvincia = CAST(IdDistrito AS INTEGER)
+                AND s.idBD = CAST(a.idSeccion AS INTEGER)
+            ) AS D
+            LEFT JOIN newC c ON c.idBD = D.idCircuito AND c.idSeccion = D.idsecNoConflict
+    ) as f
+group BY idcirNoConflict,
+    establecimiento,
+    codigo) as ff;
+
+
+
+
+
+
+
+
+
+
+INSERT INTO envios(id, date)
+    SELECT DISTINCT CAST(envio AS INTEGER),
+    fecha
+FROM votos_aux
+WHERE envio != '';
+
+INSERT INTO mesas (idBd, idEstablecimiento, electores, idEnvio)
+    SELECT DISTINCT mesa, 
+    e.id,
+    CAST(electores AS INTEGER),
+    en.id
+FROM establecimientos e JOIN votos_aux a ON e.name = a.establecimiento
+JOIN envios en ON en.id = CAST(a.envio AS INTEGER)
+WHERE mesa != '';
