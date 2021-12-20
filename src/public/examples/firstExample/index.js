118105b5079746ab40ee881fa2836e85ea9f7ec4
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-12-20 02:57:13 -0300

    added a first example

--- src/public/examples/firstExample/index.js
+++ src/public/examples/firstExample/index.js
@@ -1,101 +1,33 @@
-async function getMainNode() {
-    let width = 300;
-    let height = 502;
-    let tipoVotoPorProvincia = await d3.json("/api/provincias/tipovoto");
-    let provincias = await d3.json("/api/provincias");
-    let tiposDeVotos = await d3.json("/api/tiposDeVotos");
-    let idNulos = tiposDeVotos.find((e) => {
-        return e.name == "nulos";
-    }).id;
-
-    let data = {};
-    for (const p of provincias) {
-        data[p.name] = {
-            votos_nulos: tipoVotoPorProvincia[p.id].find((e) => {
-                return e.idtipovoto == idNulos;
-            }).votos,
-            votos_totales: tipoVotoPorProvincia[p.id].reduce((acc, curr) => {
-                return curr.votos + acc;
-            }, 0),
-        };
-    }
-    function getData(d){
-        if(d == "Tierra del Fuego") return data["Tierra del Fuego, Antártida e Islas del Atlántico Sur"]
-        else if(d == "Capital Federal") return data["Ciudad Autónoma de Buenos Aires"]
-        else return data[d]
-      }
-
-
-    let minVotos = Math.min(
-        ...Object.values(data).map((v) => v.votos_nulos / v.votos_totales)
-    );
-    let maxVotos = Math.max(
-        ...Object.values(data).map((v) => v.votos_nulos / v.votos_totales)
-    );
-
-    let geodata = await d3.json("/assets/ProvinciasArgentina.geojson");
-    let projection = d3.geoIdentity().reflectY(true).fitWidth(width, geodata);
-    let path = d3.geoPath(projection);
-
-    let div = d3.create("div");
-    let svg = div.append("svg").attr("width", width).attr("height", height);
-
-    let tooltip = div
-        .append("div")
-        .style("position", "fixed")
-        .style("visibility", "hidden")
-        .style("background-color", "white")
-        .style("border", "solid")
-        .style("border-width", "1px")
-        .style("border-color", "gray")
-        .style("border-radius", "5px")
-        .style("padding", "10px")
-        .style("width", "10em");
-    svg.selectAll("path")
-        .data(geodata.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .style("fill", function (a) {
-            let d = getData(a.properties.nombre);
-            return d3.interpolateReds(
-                d.votos_nulos / d.votos_totales / maxVotos
-            );
-        })
-        .style("stroke", "#ccc")
-        .on("mouseover", function (event, a, i) {
-            let d = getData(a.properties.nombre);
-            return tooltip
-                .text(
-                    `${a.properties.nombre}:${d.votos_nulos} votos nulos de ${
-                        d.votos_totales
-                    } (${((d.votos_nulos / d.votos_totales) * 100).toFixed(
-                        2
-                    )}%)`
-                )
-                .style("visibility", "visible");
-        })
-        .on("mousemove", function (event) {
-            let maxExtOfDiv = div.node().getBoundingClientRect().bottom;
-            let tooltipHeight = tooltip.node().getBoundingClientRect().height;
-            let maxExtOfTooltip = event.clientY + tooltipHeight;
-            if (maxExtOfDiv > maxExtOfTooltip) {
-                tooltip
-                    .style("top", event.clientY + "px")
-                    .style("left", event.clientX + "px");
-            } else {
-                tooltip
-                    .style("top", event.clientY - tooltipHeight + "px")
-                    .style("left", event.clientX + "px");
-            }
-        })
-        .on("mouseout", function (event) {
-            return tooltip.style("visibility", "hidden");
+let agrupaciones = await d3.json(`http://localhost:8080/api/agrupaciones`);
+let jsData = await d3.json(`http://localhost:8080/api/provincias/1/cargo/3/agrupacion`);
+let values = []
+for (const d of jsData) {
+    if (d.idAgrupacion != null) {
+        values.push({
+            agrupacion: agrupaciones.find(v => {
+                return v.id == d.idAgrupacion;
+            })?.name,
+            votos:d.votos
         });
-
-    return div.node();
+    }
 }
 
-d3.select("body")
-    .node()
-    .appendChild(await getMainNode());
+const spec = {
+    height:300,
+    width:400,
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    "description": "Bar chart votos del frente de izquierda",
+    "data": {
+      "values": values
+    },
+    "mark": "bar",
+    "encoding": {
+      "y": {"field": "agrupacion", "type": "nominal", "axis": {"labelAngle": 0},},
+      "x": {"field": "votos", "type": "quantitative"}
+    },
+    "config": {}
+  };
+
+
+
+vegaEmbed("#vis", spec, {mode: "vega-lite"}).then(console.log).catch(console.warn);
---
+++ src/public/examples/secondExample/index.js
@@ -0,0 +1,104 @@
+async function getMainNode() {
+    let width = 300;
+    let height = 502;
+    let tipoVotoPorProvincia = await d3.json("/api/provincias/tipovoto");
+    let provincias = await d3.json("/api/provincias");
+    let tiposDeVotos = await d3.json("/api/tiposDeVotos");
+    let idNulos = tiposDeVotos.find((e) => {
+        return e.name == "nulos";
+    }).id;
+
+    let data = {};
+    for (const p of provincias) {
+        data[p.name] = {
+            votos_nulos: tipoVotoPorProvincia[p.id].find((e) => {
+                return e.idtipovoto == idNulos;
+            }).votos,
+            votos_totales: tipoVotoPorProvincia[p.id].reduce((acc, curr) => {
+                return curr.votos + acc;
+            }, 0),
+        };
+    }
+    function getData(d) {
+        if (d == "Tierra del Fuego")
+            return data[
+                "Tierra del Fuego, Antártida e Islas del Atlántico Sur"
+            ];
+        else if (d == "Capital Federal")
+            return data["Ciudad Autónoma de Buenos Aires"];
+        else return data[d];
+    }
+
+    let minVotos = Math.min(
+        ...Object.values(data).map((v) => v.votos_nulos / v.votos_totales)
+    );
+    let maxVotos = Math.max(
+        ...Object.values(data).map((v) => v.votos_nulos / v.votos_totales)
+    );
+
+    let geodata = await d3.json("/assets/ProvinciasArgentina.geojson");
+    let projection = d3.geoIdentity().reflectY(true).fitWidth(width, geodata);
+    let path = d3.geoPath(projection);
+
+    let div = d3.create("div");
+    let svg = div.append("svg").attr("width", width).attr("height", height);
+
+    let tooltip = div
+        .append("div")
+        .style("position", "fixed")
+        .style("visibility", "hidden")
+        .style("background-color", "white")
+        .style("border", "solid")
+        .style("border-width", "1px")
+        .style("border-color", "gray")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("width", "10em");
+    svg.selectAll("path")
+        .data(geodata.features)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .style("fill", function (a) {
+            let d = getData(a.properties.nombre);
+            return d3.interpolateReds(
+                d.votos_nulos / d.votos_totales / maxVotos
+            );
+        })
+        .style("stroke", "#ccc")
+        .on("mouseover", function (event, a, i) {
+            let d = getData(a.properties.nombre);
+            return tooltip
+                .text(
+                    `${a.properties.nombre}:${d.votos_nulos} votos nulos de ${
+                        d.votos_totales
+                    } (${((d.votos_nulos / d.votos_totales) * 100).toFixed(
+                        2
+                    )}%)`
+                )
+                .style("visibility", "visible");
+        })
+        .on("mousemove", function (event) {
+            let maxExtOfDiv = div.node().getBoundingClientRect().bottom;
+            let tooltipHeight = tooltip.node().getBoundingClientRect().height;
+            let maxExtOfTooltip = event.clientY + tooltipHeight;
+            if (maxExtOfDiv > maxExtOfTooltip) {
+                tooltip
+                    .style("top", event.clientY + "px")
+                    .style("left", event.clientX + "px");
+            } else {
+                tooltip
+                    .style("top", event.clientY - tooltipHeight + "px")
+                    .style("left", event.clientX + "px");
+            }
+        })
+        .on("mouseout", function (event) {
+            return tooltip.style("visibility", "hidden");
+        });
+
+    return div.node();
+}
+
+d3.select("body")
+    .node()
+    .appendChild(await getMainNode());
