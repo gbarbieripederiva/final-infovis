e2fcc3c63b9b1d57b2917589a32e640380317589
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-12-19 22:57:21 -0300

    fix insert

--- initDatabase/scripts/posInsertProcess.sql
+++ initDatabase/scripts/posInsertProcess.sql
@@ -95,52 +95,41 @@ INSERT INTO tiposvotos(tipoVoto)
     WHERE tipoVoto != '';
 
 
-WITH newM AS (
-    SELECT idMesa AS idmesaNoConflict,
-        idBd,
-        idEstablecimiento,
-        electores,
-        idEnvio
-    FROM mesas
-),
-newTV AS (
-    SELECT idTipoVoto AS idtipovotoNoConflict,
-        tipoVoto
-    FROM tiposvotos
-),
-newE AS (
-    SELECT idEstablecimiento AS idestablecimientoNoConflict,
-        establecimiento, 
-        idCircuito as idcirinest, 
-        codigo
-    FROM establecimientos
-),
-newC AS (
-    SELECT idCircuito AS idcirNoConflict,
-        idbd,
-        idSeccion AS idsecincirNoConflict
-    FROM circuitos
-),
-newS AS (
-    SELECT idSeccion AS idsecNoConflict,
-        idbd,
-        idProvincia,
-        seccion
-    FROM secciones
-)
-INSERT INTO votos(idAgrupacion, idCargo, idMesa, idTipoVoto, votes)
-    SELECT CAST(idAgrupacionInt AS INTEGER), CAST(idCargo AS INTEGER), idmesaNoConflict, idtipovotoNoConflict, CAST(votos AS INTEGER)
-    FROM 
-        ((((((SELECT idAgrupacionInt, idCargo, mesa, tipoVoto, votos, establecimiento, codigo, idCircuito, idSeccion, idDistrito
-        FROM votos_aux) AS A
-        LEFT JOIN newTV tv ON tv.tipoVoto = A.tipoVoto) AS ATV
-        LEFT JOIN newC c ON c.idbd = ATV.idCircuito and c.idsecincirNoConflict = CAST(ATV.idSeccion AS INTEGER)) AS ATVC
-        LEFT JOIN newS s ON s.idbd = ATVC.idsecincirNoConflict and s.idProvincia = CAST(ATVC.idDistrito AS INTEGER)) AS ATVCS
-        LEFT JOIN newE e ON e.establecimiento = ATVCS.establecimiento and e.codigo = CAST(ATVCS.codigo AS INTEGER) and e.idcirinest = ATVCS.idcirNoConflict) AS ATVCSE
-        LEFT JOIN newM m ON m.idBD = ATVCSE.mesa AND ATVCSE.idestablecimientoNoConflict = m.idEstablecimiento) AS ATVCSEM;
     
 INSERT INTO agrupacionProvincia(idAgrupacion, idProvincia)
     SELECT DISTINCT CAST(idAgrupacionInt AS INTEGER), 
                     CAST(idDistrito AS INTEGER)
     FROM votos_aux
     WHERE idAgrupacionInt != '';
+
+
+-- This is needed to optimize insertion
+CREATE MATERIALIZED VIEW pscem AS
+SELECT idProvincia, provincia, idSeccion, idSeccionBD, idCircuito, idCircuitoBD, psce.idEstablecimiento, establecimiento, codigo, idMesa, m.idBD AS idMesaBD
+FROM (
+    SELECT idProvincia, provincia, idSeccion, idSeccionBD, idCircuito, idCircuitoBD, e.idEstablecimiento, establecimiento, codigo
+    FROM (
+        SELECT idProvincia, provincia, ps.idSeccion, idSeccionBD, idCircuito, c.idBD as IdCircuitoBD
+        FROM (
+            SELECT idProvincia, provincia, idSeccion, s.idBD as idSeccionBD
+            FROM provincias AS p
+            NATURAL JOIN secciones s) AS ps
+        JOIN circuitos c ON c.idSeccion = ps.idSeccion) AS psc
+    NATURAL JOIN establecimientos e) AS psce
+JOIN mesas m ON m.idEstablecimiento = psce.idEstablecimiento;
+
+CREATE MATERIALIZED VIEW votos_casted AS
+SELECT CAST(idAgrupacionInt AS INTEGER), CAST(idCargo AS INTEGER), mesa, tipoVoto, CAST(votos AS INTEGER), establecimiento, CAST(codigo AS INTEGER), idCircuito, CAST(idSeccion AS INTEGER), CAST(idDistrito AS INTEGER)
+        FROM votos_aux;
+
+INSERT INTO votos(idAgrupacion, idCargo, idMesa, idTipoVoto, votes)
+SELECT idAgrupacionInt, idCargo, ATVPSCEM.idMesa, idtipovoto, votos
+FROM 
+((votos_casted A JOIN tiposvotos tv ON tv.tipoVoto = A.tipoVoto) AS ATV
+JOIN pscem ON 
+            ATV.mesa = pscem.idMesaBD 
+            AND ATV.establecimiento = pscem.establecimiento 
+            AND ATV.codigo = pscem.codigo 
+            AND ATV.idCircuito = pscem.idCircuitoBD 
+            AND ATV.idSeccion = pscem.idSeccionBD
+            AND ATV.idDistrito = pscem.idProvincia) AS ATVPSCEM;
